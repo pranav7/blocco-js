2d6de1ecb1db63b3f842cc0c352fe1992db9f305
Pranav <pranav.singh@intercom.io>
2021-09-15 16:55:41 +0100

    Grid Spike (#3)
    * Grid Spike
    
    * Checkpoint
    
    * Update

---
+++ app/components/grid.js
@@ -0,0 +1,37 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import moment from 'moment';
+import { Calendar } from '@fullcalendar/core';
+
+class GridConfig {
+  @tracked start;
+  @tracked end;
+
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+}
+
+export default class GridComponent extends Component {
+  gridConfig;
+
+  constructor() {
+    super(...arguments);
+
+    this.gridConfig = new GridConfig(9, 18);
+  }
+
+  get timeSlots() {
+    let slots = [];
+    let startTime = moment(this.gridConfig.start, 'HH::mm');
+    let endTime = moment(this.gridConfig.end, 'HH::mm');
+
+    while (startTime <= endTime) {
+      slots.push(startTime.format('h:mm a'));
+      startTime.add(60, 'minutes');
+    }
+
+    return slots;
+  }
+}
